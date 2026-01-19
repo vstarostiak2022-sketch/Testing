539064831a77e290486fc7a2dc7ed4a52b9cca1e
Adam <adereshchul2022@itcollege.lviv.ua>
2026-01-19 10:06:12 +0200

    commit

---
+++ examen/main.py
@@ -0,0 +1,8 @@
+def get_file_extension(filename: str) -> str:
+    if filename.startswith('.') and filename.count('.') == 1:
+        return ''
+    
+    parts = filename.rsplit('.', 1)
+    if len(parts) == 2 and parts[1]:
+        return parts[1]
+    return ''
